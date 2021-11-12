c941d40daf4d37cfde023103aae922a2a4e61767
Devhocke <71835476+DevHocke@users.noreply.github.com>
2021-11-12 11:28:25 +0100

    Test1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace EnvvironmentTesting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! testar1");
         }
     }
 }
